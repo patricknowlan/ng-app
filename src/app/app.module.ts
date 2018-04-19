229c84d54acc0cb8bed1eac76eb3eab34f6a44aa
cjk101812 <cullen@540.co>
2018-04-19 10:22:55 -0600

    Adding map to main view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule }    from '@angular/common/http';
 import {MatListModule} from '@angular/material';
 
-
+import { NguiMapModule} from '@ngui/map';
 
 import { AppComponent } from './app.component';
 import { VineService } from './vine.service';
+import { MapComponent } from './map/map.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo:'/home', pathMatch: 'full' },
   { path: 'home', component: AppComponent }
+  // { path: 'home', component: AppComponent,
+  //   children: [
+  //     { path: 'wineries', component: WineryComponent },
+  //     { path: 'breweries', component: BreweryComponent }
+  //   ]
+  // }
 ];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +34,8 @@ const appRoutes: Routes = [
       { enableTracing: false } // <-- debugging purposes only
     ),
     HttpClientModule,
-    MatListModule
+    MatListModule,
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyCs5uneLE643PYoZugfiH2XtyW492lamRc'})
   ],
   providers: [VineService],
   bootstrap: [AppComponent]
